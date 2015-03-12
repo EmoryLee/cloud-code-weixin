a1724aea8897da231c743d9083850a63627cbf91
EmoryLee <EmoryLee@users.noreply.github.com>
2015-03-12 17:56:22 +0800

    Update main.js

--- cloud/main.js
+++ cloud/main.js
@@ -2,5 +2,10 @@ require("cloud/app.js");
 // Use AV.Cloud.define to define as many cloud functions as you want.
 // For example:
 AV.Cloud.define("hello", function(request, response) {
-  response.success("Hello world!");
+  var name = request.params.name;
+  if (name) {
+    response.success("Hello " + name);
+  } else {
+    response.error('name?');
+  }
 });
